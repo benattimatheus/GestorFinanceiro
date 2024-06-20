4f42ee6539344401b2cbd5814166f14880336e65
Luciano Franzoi Filho <137215449+Luciano-Franzoi-Filho@users.noreply.github.com>
2024-06-19 21:28:26 -0300

    Update Database.php
    mudar caminho relativo do banco

--- src/database/Database.php
+++ src/database/Database.php
@@ -7,7 +7,7 @@ class Database
     private function __construct()
     {
         if (is_null(self::$conn)) {
-            self::$conn = new PDO(dsn: 'sqlite:Financas.db', options:[
+            self::$conn = new PDO(dsn: 'sqlite:../database/Financas.db', options:[
                 PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,   
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
