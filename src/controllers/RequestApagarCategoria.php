7c2c8074261f3e99689ecced60b898f0f5fec0ba
Luciano Franzoi Filho <lucianofilhodombosco@gmail.com>
2024-06-25 14:43:57 -0300

    update apagar categoria, apagar junto as receitas/despesar relacionadas

--- src/controllers/RequestApagarCategoria.php
+++ src/controllers/RequestApagarCategoria.php
@@ -7,12 +7,24 @@ class RequestApagarCategoria {
     {
         try {
             $pdo = Database::getConn();
+            $pdo->beginTransaction();
+
+            $stmt = $pdo->prepare("DELETE FROM receitas WHERE categoria = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
+            $stmt = $pdo->prepare("DELETE FROM despesas WHERE categoria = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
             $stmt = $pdo->prepare("DELETE FROM categoria WHERE id = :id");
             $stmt->bindParam(':id', $id);
             $stmt->execute();
 
+            $pdo->commit();
             echo json_encode(['success' => true]);
         } catch (PDOException $e) {
+            $pdo->rollBack();
             echo json_encode(['error' => 'Erro ao conectar ao banco de dados: ' . $e->getMessage()]);
         }
     }
